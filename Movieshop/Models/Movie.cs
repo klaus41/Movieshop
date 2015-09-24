5db34fb4db0debafe121eb352211eab2927b9d2e
unknown <klausgaarde@live.dk>
2015-09-24 18:05:10 +0200

    Error fix

---
+++ Movieshop/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movieshop.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+}
